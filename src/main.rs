41a4cbf6f0a7cf20a77ee80ac35366b1631ab63b
Daniel Atkinson <hourd.tasa@gmail.com>
2015-10-23 17:15:40 +0100

    added default redirection on and routed via GET

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate iron;
+#[macro_use]
+extern crate router;
+extern crate url;
+
+use iron::prelude::*;
+use iron::{ Url, status, Headers };
+use iron::request::{ Body };
+use iron::modifiers::Redirect;
+use router::Router;
+use url::Host;
+
 fn main() {
-    println!("Hello, world!");
+    let router = router!(
+        get "/" => redirect_handler,
+        get "*" => redirect_handler
+    );
+
+    Iron::new(router).http("0.0.0.0:4000").expect("Failed to start server");
+
+}
+
+fn redirect_handler(req: &mut Request) -> IronResult<Response> {
+    // Create a mutable clone of the request url
+    let ref incoming_url_ref = req.url;
+    let mut incoming_url = incoming_url_ref.clone();
+    // Change url host to a looked up address while maintaining the rest of the url
+    // TODO: Actually look up the correct address to use
+    incoming_url.host = Host::Domain(String::from("github.com"));
+    // Setting port ot 80 as default as port is currently unknown
+    // TODO: look up correct port to use
+    incoming_url.port = 80;
+    Ok(Response::with((status::TemporaryRedirect, Redirect(incoming_url))))
 }
